cddfe44a7fda88d0570cc7dc3f2e38dbbc94017d
vinikjkkj <contact@vinicius.email>
2026-08-30 17:26:20 -0300

    docs(voipfix): the lab, its environment, and what each probe settles

---
+++ lab/env.sh
@@ -0,0 +1,7 @@
+export ZIG_GLOBAL_CACHE_DIR='<blocks>\voipfix\.zig'
+export ZIG_LOCAL_CACHE_DIR='<blocks>\voipfix\.zig'
+export SCRIPTC_CACHE_DIR='<blocks>\voipfix\.cache'
+export TMP='<blocks>\voipfix-tmp' TEMP='<blocks>\voipfix-tmp' TMPDIR='<blocks>\voipfix-tmp'
+export SCRIPTC_CC=zigcc SCRIPTC_TEST_CC='zig cc' SCRIPTC_TARGET=x86_64-windows-gnu
+export SCRIPTC_TEST_WORKERS=3
+export PATH="<zapo-work>/tools/zig:$PATH"
